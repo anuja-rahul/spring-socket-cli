e8296f8e0e2c8910d242c6ba2a0c4eae564946b8
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2025-02-02 22:54:04 +0530

    first commit

---
+++ SocketCli/src/main/java/com/example/socketcli/SocketCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketCliApplication.class, args);
+    }
+
+}
